4b68ee75d60af580cb3c6d19cd77488995b0235c
Ryan Lucchese <ryan.lucchese@write-c.com>
2013-11-23 11:28:20 -0700

    beginning implementation of test case list

--- src/potency.c
+++ src/potency.c
@@ -1,1 +1,67 @@
 #include "potency.h"
+
+#include <stdlib.h>
+
+typedef struct _potency_test_case_list
+{
+	potency_test_case* testCase;
+	struct _potency_test_case_list* next;
+	struct _potency_test_case_list* prev;
+} potency_test_case_list;
+
+static potency_test_case_list* testCaseHead = NULL;
+
+// static int potency_add_test_case( const char* name, const char* description, potency_test_case_function _run )
+// {
+// 	potency_test_case* newTestCase = calloc(1, sizeof(*newTestCase));
+// 	newTestCase->name = name;
+// 	newTestCase->description = description;
+// 	newTestCase->run = _run;
+
+// 	if (testCaseHead == NULL)
+// 	{
+// 		testCaseHead = calloc(1, sizeof(*testCaseHead));
+// 		testCaseHead->testCase = newTestCase;
+// 		return 0;
+// 	}
+
+// 	potency_test_case_list* testCaseEnd = testCaseHead;
+// 	while (testCaseEnd->next != NULL)
+// 	{
+// 		testCaseEnd = testCaseEnd->next;
+// 	}
+
+// 	potency_test_case_list* newTestCaseList = calloc(1, sizeof(*newTestCaseList));
+// 	newTestCaseList->testCase = newTestCase;
+// 	newTestCaseList->prev = testCaseEnd;
+// 	testCaseEnd->next = newTestCaseList;
+
+// 	return 0;
+// }
+
+static void potency_cleanup_test_cases()
+{
+	potency_test_case_list* currentTestCaseList = testCaseHead;
+
+	while (currentTestCaseList != NULL)
+	{
+		free(currentTestCaseList->testCase);
+
+		potency_test_case_list* toBeFreed = currentTestCaseList;
+		currentTestCaseList = toBeFreed->next;
+		free(toBeFreed);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	(void)argc;
+	(void)argv;
+	potency_cleanup_test_cases();
+	return 0;
+}
+
+TEST_CASE("exampleTestCase", "Just here for testing test cases")
+{
+	CHECK(1 == 1);
+}
--- src/potency.h
+++ src/potency.h
@@ -1,29 +1,46 @@
 #ifndef potency_h
 #define potency_h
 
+#include <stdint.h>
+#include <stdio.h>
+
 // create a unique name by appending the line number to a name
 #define potency_unique_name_line2( name, line ) name##line
 #define potency_unique_name_line( name, line ) potency_unique_name_line2( name, line )
 #define potency_unique_name( name ) potency_unique_name_line( name, __LINE__ )
 
 // potency_test_case
-typedef (void(*)()) potency_test_case_function;
+struct _potency_test_case;
+typedef void(*potency_test_case_function)( struct _potency_test_case* );
 
 // struct _potency_test_case is used to contain all data associated with a test case
 typedef struct _potency_test_case
 {
-	const char* testCaseName;
-	const char* testCaseDescription;
-	potency_test_case_function runTestCase;
-	size_t numberOfAssertions;
-	size_t numberOfPassedAssertions;
-	double timeToRun;
+	const char* name;
+	const char* description;
+	potency_test_case_function run;
+	uint32_t assertions;
+	uint32_t passedAssertions;
+	double runTime;
 } potency_test_case;
 
+// static int potency_add_test_case( const char* name, const char* description, const potency_test_case_function _run );
+
 #define TEST_CASE( name, description )\
-static void potency_unique_name( potency_test_case_implementation_ )();\
-potency_add_test_case( name, description, &potency_unique_name( potency_test_case_implementation_ ) ); \
-static void potency_unique_name( potency_test_case_implmementation_ )()
+static void potency_unique_name( potency_test_case_implementation_ )(potency_test_case* testCase); \
+static void potency_unique_name( potency_test_case_implementation_ )(potency_test_case* testCase)
+//static const int potency_unique_name( potency_unused_ ) = potency_add_test_case( name, description, &potency_unique_name(potency_test_case_implementation_)); 
+
+#define CHECK(expression)\
+	testCase->assertions++;\
+	if (expression)\
+	{\
+		testCase->passedAssertions++;\
+	}\
+	else\
+	{\
+		printf("*** assertion failed: %s\n", #expression);\
+	}
 
 
 #endif // !potency_h
