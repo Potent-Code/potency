21094026a9332551e404d4e7e1f782574e38552a
Ryan Lucchese <ryan.lucchese@write-c.com>
2013-11-23 20:26:00 -0700

    build a shared lib and test binary

--- bam.lua
+++ bam.lua
@@ -1,12 +1,16 @@
 -- important paths
 sourcePath = "src/"
 objectPath = "obj/"
+testPath = "src/test/"
+testObjectPath = "obj/test/"
 
 -- set up compiler settings
 settings = NewSettings()
 
 -- full warning level, treat warnings as errors
 settings.cc.flags:Add("-Wall -Wextra -Werror -Wno-unused-function")
+-- use fPIC for making a shared object
+settings.cc.flags:Add("-fPIC")
 
 -- output objects to obj directory
 ExecuteSilent("mkdir obj")
@@ -17,3 +21,26 @@ end
 -- collect sources and compile
 source = Collect(sourcePath .. "*.c")
 objects = Compile(settings, source)
+
+-- make shared objects
+libpotency = SharedLibrary(settings, "libpotency", objects)
+
+-- build test binary
+settings = NewSettings()
+-- full warning level, treat warnings as errors
+settings.cc.flags:Add("-Wall -Wextra -Werror -Wno-unused-function")
+-- use fPIC for making a shared object
+settings.cc.flags:Add("-fPIC")
+settings.cc.includes:Add(sourcePath)
+settings.link.libpath:Add(".")
+settings.link.libs:Add("potency")
+
+-- output objects to obj directory
+ExecuteSilent("mkdir obj/test")
+settings.cc.Output = function(settings, input)
+	return testObjectPath .. PathFilename(PathBase(input))
+end
+
+source = Collect(testPath .. "*.c")
+objects = Compile(settings, source)
+exe = Link(settings, "test_potency", objects)
---
+++ src/test/test2_potency.c
@@ -0,0 +1,7 @@
+#include <potency.h>
+
+TEST_CASE("testCase3", "Another test case")
+{
+	CHECK(1 != 0);
+	CHECK(1 == 0);
+}
