affcf3dba4ea756f17a585fe5cc948674787ea9d
Ryan Lucchese <ryan.lucchese@write-c.com>
2013-11-24 12:38:04 -0700

    added xml format

--- src/report_xml.c
+++ src/report_xml.c
@@ -1,7 +1,17 @@
 #include "potency.h"
+#include "report.h"
 #include "report_xml.h"
 
 void prepare_report_xml()
 {
-	// TODO
+	test_statistics stats;
+	collect_statistics(&stats);
+
+	printf("<statistics>\n");
+	printf("\t<assertions>%u</assertions>\n", stats.assertions);
+	printf("\t<passedassertions>%u</passedassertions>\n", stats.passedAssertions);
+	printf("\t<failedassertions>%u</failedassertions>\n", stats.failedAssertions);
+	printf("\t<successpercentage>%.2f</successpercentage>\n", stats.successPercentage);
+	printf("\t<testcases>%u</testcases>\n", stats.testCases);
+	printf("</statistics>\n");
 }
