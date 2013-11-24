c0ebf25801e52eda0d26b87c90adee6f92dd8b69
Ryan Lucchese <ryan.lucchese@write-c.com>
2013-11-24 12:21:39 -0700

    added json output format

--- src/report_json.c
+++ src/report_json.c
@@ -1,7 +1,19 @@
 #include "potency.h"
+#include "report.h"
 #include "report_json.h"
 
 void prepare_report_json()
 {
-	// TODO
+	test_statistics stats;
+	collect_statistics(&stats);
+
+	printf("{\n");
+	printf("\t\"statistics\": {\n");
+	printf("\t\t\"testCases\":%u,\n", stats.testCases);
+	printf("\t\t\"assertions\":%u,\n", stats.assertions);
+	printf("\t\t\"passedAssertions\":%u,\n", stats.passedAssertions);
+	printf("\t\t\"failedAssertions\":%u,\n", stats.failedAssertions);
+	printf("\t\t\"successPercentage\":%.2f\n", stats.successPercentage);
+	printf("\t}\n");
+	printf("}\n");
 }
