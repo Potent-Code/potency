aad39a63b3a469f0f8ad1f3074a7c3d1293a58a6
Ryan Lucchese <ryan.lucchese@write-c.com>
2013-11-30 20:43:34 -0700

    begin implementing benchmarks

---
+++ src/benchmark.c
@@ -0,0 +1,16 @@
+#include "benchmark.h"
+
+#include <stdlib.h>
+#include <sys/time.h>
+
+double potency_get_time()
+{
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL))
+	{
+		return 0;
+	}
+
+	return (double)tv.tv_sec + ((double)tv.tv_usec * 1.0e-6);
+}
---
+++ src/benchmark.h
@@ -0,0 +1,6 @@
+#ifndef benchmark_h
+#define benchmark_h
+
+extern double potency_get_time();
+
+#endif // !benchmark_h
