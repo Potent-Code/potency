724d9c13926b9567b080b79abae3f53b3cb2c8d1
Ryan Lucchese <ryan.lucchese@write-c.com>
2015-03-22 18:54:34 -0600

    implemented test case list for json

--- src/report_json.h
+++ src/report_json.h
@@ -8,6 +8,7 @@ extern void potency_print_report_json();
 extern void potency_print_report_footer_json();
 extern void potency_print_assertion_json(potency_test_case* testCase, bool passed, const char* file, const uint32_t line, const char* expression);
 extern void potency_print_requirement_json(potency_test_case* testCase, bool passed, const char* file, const uint32_t line, const char* expression);
+extern void potency_print_list_json();
 
 #endif // !potency_report_json_h
 
